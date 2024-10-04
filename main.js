6315d8bd81b71ab740f7eab05a11e819554a10be
Mithila-tesing <138238311+Mithila-tesing@users.noreply.github.com>
2024-10-05 00:28:01 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+// 20241004235546
+// https://cdn.flashtalking.com/88885/sebpoTest_fontRL/3369_inPlay.json
+
+{
+  "moneyline_marketID": "754.99954619",
+  "spread_marketID": "754.99954617",
+  "eventID": "33624502",
+  "marketStartTime": "2024-10-06T17:00:00.000Z",
+  "inplay": true,
+  "home_name": "Houston Texans",
+  "home_decimal": 1.9090909090909092,
+  "home_numerator": 100,
+  "home_denominator": 110,
+  "home_moneyline": -110,
+  "home_spread_denominator": 102,
+  "home_spread_numerator": 100,
+  "home_spread_handicap": -1.5,
+  "away_name": "Buffalo Bills",
+  "away_decimal": 1.9433962264150944,
+  "away_numerator": 100,
+  "away_denominator": 106,
+  "away_moneyline": -106,
+  "away_spread_denominator": 120,
+  "away_spread_numerator": 100,
+  "away_spread_handicap": 1.5,
+  "away_logo_horizontal": "https://cdn.flashtalking.com/feeds/fanduel/FBI-5560/images/left/bills_square_away.png",
+  "away_logo_vertical": "https://cdn.flashtalking.com/feeds/fanduel/FBI-5560/images/top/bills_liveodds_away.png",
+  "home_logo_horizontal": "https://cdn.flashtalking.com/feeds/fanduel/FBI-5560/images/right/texans_square_home.png",
+  "home_logo_vertical": "https://cdn.flashtalking.com/feeds/fanduel/FBI-5560/images/bottom/texans_liveodds_home.png"
+}
